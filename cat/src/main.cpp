ff7e1c9bfe8a9efb2812b551c97c54298c3a9a90
rinat <imatdRA@yandex.ru>
2023-10-07 21:56:14 +0300

    Opengl lesson 6

--- cat/src/main.cpp
+++ cat/src/main.cpp
@@ -5,46 +5,60 @@
 #include <SFML/OpenGL.hpp>
 #include <math.h>
 
-void ShowObj(float scale, float transX, float transY) {
-    glLoadIdentity();           // восстанавливаем систему координат
-    glScalef(scale, scale, 1);
-    glTranslatef(transX, transY, 0);
-    glBegin(GL_TRIANGLE_FAN);
-    glColor3f(0, 0, 0); glVertex2f(0, 0);
-    glColor3f(1, 0, 0); glVertex2f(0.2, 0);
-    glColor3f(0, 0, 1); glVertex2f(0.2, 0.2);
-    glColor3f(0, 0, 1); glVertex2f(0.1, 0.3);
-    glColor3f(0, 1, 0); glVertex2f(0, 0.2);
-    glEnd();
-    glBegin(GL_TRIANGLE_FAN);
-    glColor3f(1, 1, 1); glVertex2f(0.05, 0.05);
-    glColor3f(1, 1, 1); glVertex2f(0.05, 0.15);
-    glColor3f(1, 1, 1); glVertex2f(0.15, 0.15);
-    glColor3f(1, 1, 1); glVertex2f(0.15, 0.05);
-    glEnd();
-}
-void SunObj() {
-    static float theta = 0.0f;
-    glLoadIdentity();           // восстанавливаем систему координат
-    glTranslatef(0, -0.7, 0);   // опускаем систему координат по оси y
-    glRotatef(theta, 0.0f, 0.0f, 1.0f); // поворачиваем систему координат на угол
-    glTranslatef(-1.5, 0, 0);    // смещаем по оси x
+POINTFLOAT* mas = nullptr;
+int cnt;
+float scaleY = 1;
+float curX;
+
+void DrawOs(float alfa) {
+    static float d = 0.05;
     glPushMatrix();
-    float x, y;
-    float cnt = 50;
-    float l = 0.1;
-    float a = M_PI * 2 / cnt;
-    glBegin(GL_TRIANGLE_FAN);
-    glColor3f(0.97, 0.8, 0);
-    glVertex2f(0, 0);
-    for (int i = -1; i < cnt; i++) {
-        x = sin(a * i) * l;
-        y = cos(a * i) * l;
-        glVertex2f(x, y);
+    glRotatef(alfa, 0, 0, 1);
+    {
+        glBegin(GL_LINES);
+        glVertex2f(-1, 0);
+        glVertex2f(1, 0);
+        glVertex2f(1, 0);
+        glVertex2f(1 - d, 0 + d);
+        glVertex2f(1, 0);
+        glVertex2f(1 - d, 0 - d);
+        glEnd();
     }
+    glPopMatrix();
+}
+
+void Init(float start, float finish, int count)
+{
+    cnt = count;
+    mas = new POINTFLOAT[cnt];
+    float dx = (finish - start) / (cnt - 1);
+    for (int i = 0; i < cnt; i++) 
+    {
+        mas[i].x = start;
+        mas[i].y = sin(start);
+        start += dx;
+    }
+}
+void Show()
+{
+    float sx = 2.0 / (mas[cnt - 1].x - mas[0].x);
+    float dx = (mas[cnt - 1].x + mas[0].x) * 0.5;
+    glPushMatrix();
+    glScalef(sx, scaleY, 1);
+    glTranslatef(-dx, 0, 0);
+    glBegin(GL_LINE_STRIP);
+    for (int i = 0; i < cnt; i++) 
+        glVertex2f(mas[i].x, mas[i].y);
     glEnd();
     glPopMatrix();
-    theta += -0.3f;
+}
+
+void Add(float x, float y) 
+{
+    for (int i = 1; i < cnt; i++)
+        mas[i - 1] = mas[i];
+    mas[cnt - 1].x = x;
+    mas[cnt - 1].y = y;
 }
 
 int main()
@@ -58,7 +72,8 @@ int main()
     window.setActive(true);
 
     // load resources, initialize the OpenGL states, ...
-
+    curX = 10;
+    Init(10, curX, 100);
     // run the main loop
     bool running = true;
     while (running)
@@ -79,6 +94,14 @@ int main()
                 if (event.key.code == sf::Keyboard::Escape)
                     running = false;
                 break;
+            case sf::Event::MouseWheelScrolled:
+                if ((int)event.mouseWheelScroll.delta > 0)
+                    scaleY *= 1.5;
+                else
+                    scaleY *= 0.7;
+                if (scaleY < 0.02)
+                    scaleY = 0.02;
+                break;
             default:
                 break;
             }
@@ -87,10 +110,17 @@ int main()
         // clear the buffers
         glClearColor(0.7f, 1.0f, 0.7f, 0.0f); // цвет очистки экрана
         glClear(GL_COLOR_BUFFER_BIT);
-        ShowObj(0.5, 0.2, 0);
-        ShowObj(1, -0.2, 0);
-        SunObj();
-        
+        glLoadIdentity();
+        glLineWidth(2);
+        glColor3f(1, 0, 0);
+        DrawOs(0);
+        glColor3f(0, 1, 0);
+        DrawOs(90);
+
+        curX += 0.1;
+        Add(curX, sin(curX));
+        glColor3f(0, 1, 1);
+        Show();
         // end the current frame (internally swaps the front and back buffers)
         window.display();
     }
